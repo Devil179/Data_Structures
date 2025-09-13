3524f88a11dfd1c7ebf7b30452a3a4070a8129ec
Sparsh Agarwal <a.sparsh@op.iitg.ac.in>
2025-09-13 13:53:59 +0530

    prac5-middle no.

--- practice/prac5.java
+++ practice/prac5.java
@@ -1,3 +1,15 @@
 public class prac5 {
-    
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int n = arr.length;
+        if (n % 2 == 0) {
+            int mid1 = arr[(n / 2) - 1];
+            int mid2 = arr[n / 2];
+            System.out.println("Middle elements: " + mid1 + " and " + mid2);
+        } 
+        else {
+            int mid = arr[n / 2];
+            System.out.println("Middle element: " + mid);
+        }
+    }
 }
