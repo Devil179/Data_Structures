e3f04cb9a89d58dbeadb4372dbb73e3471bd007d
Sparsh Agarwal <a.sparsh@op.iitg.ac.in>
2025-09-24 12:28:43 +0530

    fibonacci series final

--- practice/prac12.java
+++ practice/prac12.java
@@ -1,12 +1,15 @@
 public class prac12 {
-    static int recur_fib(int n){
-        if (n == 0) return 0;
-        if (n == 1) return 1;
+    static int recur_fib(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
         return recur_fib(n - 1) + recur_fib(n - 2);
     }
 
-    static int iter_fib(int n){
-        if (n < 2) return n;
+    static int iter_fib(int n) {
+        if (n < 2)
+            return n;
         int a = 0, b = 1;
         for (int i = 2; i <= n; i++) {
             int c = a + b;
@@ -16,13 +19,15 @@ public class prac12 {
         return b;
     }
 
-    static void print_fib(int n){
-        if (n <= 0){
+    static void print_fib(int n) {
+        if (n <= 0) {
             System.out.println();
             return;
         }
-        if (n >= 1) System.out.print(0);
-        if (n >= 2) System.out.print(" 1");
+        if (n >= 1)
+            System.out.print(0);
+        if (n >= 2)
+            System.out.print(" 1");
         int a = 0, b = 1;
         for (int i = 3; i <= n; i++) {
             int c = a + b;
@@ -32,10 +37,12 @@ public class prac12 {
         }
         System.out.println();
     }
-    static void reversed(int n){
-        if (n < 0) return;
+
+    static void reversed(int n) {
+        if (n < 0)
+            return;
         System.out.print(n);
-        if (n > 0){
+        if (n > 0) {
             System.out.print(" ");
             reversed(n - 1);
         } else {
