2d2d1e8d075cb8a509f56d3b2f0bdd8d62c1239b
Sparsh Agarwal <a.sparsh@op.iitg.ac.in>
2025-09-27 10:57:26 +0530

    feat: implement factorial function and main method

--- practice/prac13.java
+++ practice/prac13.java
@@ -1,9 +1,12 @@
 public class prac13 {
+    static int moveCount = 0;
+
     static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
         if (n == 0) {
             return;
         }
         towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+        moveCount++;
         System.out.println("Move disk " + n + " from rod " + from_rod + " to rod " + to_rod);
         towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
     }
@@ -13,6 +16,8 @@ public class prac13 {
         int N = 3;
 
         // A, B and C are names of rods
+        moveCount = 0;
         towerOfHanoi(N, 'A', 'C', 'B');
+        System.out.println("Total moves: " + moveCount);
     }
 }
