329660d5fcfe3f659c77cd4ceb63bfb88030f393
Sparsh Agarwal <a.sparsh@op.iitg.ac.in>
2025-09-27 11:08:23 +0530

    feat

--- practice/tri.java
+++ practice/tri.java
@@ -3,7 +3,7 @@ public class tri {
         int n = 4;
         for (int i = 1; i <= n; i++) {
             for ( int j = 1; j <= i; j++) {
-                System.out.print(i+" ");
+                System.out.print("*"+" ");
             }
             System.out.println();
             
